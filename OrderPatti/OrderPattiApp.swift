eae176de571dcc052b12c10a539d0d4029023286
Vikash sahu <vikash_sahu_s_a_technologies@whirlpool.com>
2024-08-20 18:12:09 +0530

    Initial Commit

---
+++ OrderPatti/OrderPattiApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderPattiApp.swift
+//  OrderPatti
+//
+//  Created by Vikash Sahu on 20/08/24.
+//
+
+import SwiftUI
+
+@main
+struct OrderPattiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
